c5830586d163e43d18844aae4932f2f1d4686925
Devin Gaffney <itsme@devingaffney.com>
2017-02-22 20:58:16 -0500

    updates

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 #Scotland Yard Network Analysis
-
-![Map Of Scotland Yard](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/successful_transits.png)
-
 This is a repository that explores the network analysis attributes of the game [Scotland Yard](https://en.wikipedia.org/wiki/Scotland_Yard_(board_game)).
 
 
@@ -26,9 +23,32 @@ If you're a spy, you want to try to get to these positions - in two turns, this 
 
 As the spy moves around the board, they are transiting the various stations, or in network terms, nodes. Some nodes are connected to many other nodes, which means that there are many ways to escape that particular node, but also many ways to get to that node - if any of the six adversaries land on the node the spy currently sits on, the spy loses. At every point, each player has several methods of transit - taxi, bus, metro, or in the case of the spy, a black ticket that can stand in for any of those three OR an additional ferry route. The code in random\_game.py simulates as close to non-decision making as possible for the players - they simply move to an available position proportional to the tickets left for each type as they are available for the position. The code in strategy\_game.py is a bit more involved:
 
-In the beginning of the game, Adversaries have no priors on the location of the spy, so they move around randomly on the board (of course, they could work together to increase maximal coverage, but right now these agents act independent of one another). Once they know the location of the spy, they use an implementation of [Yen's algorithm](https://en.wikipedia.org/wiki/Yen's_algorithm) to contemplate several potential shortest paths towards the spy's known location, and then select the shortest one available that satisfies their current ticket alotment. On their next turn, they create a hypothesis as to the direction of the spy, and assume the spy has randomly walked to another node (future feature update here is to have them hypothesize that the spy has moved to a node furthest away from this agent). They keep updating their guess at the path the spy is randomly walking, and keep using Yen's algorithm to approach their current hypothesis. When the spy is revealed in a later round, this guess is replaced with the reality, and the process continues.
+In the beginning of the game, Adversaries have no priors on the location of the spy, so they move around randomly on the board (of course, they could work together to increase maximal coverage, but right now these agents act independent of one another). Once they know the location of the spy, they use an implementation of [Yen's algorithm](https://en.wikipedia.org/wiki/Yen's_algorithm) to contemplate several potential shortest paths towards the spy's known location, and then select the shortest one available that satisfies their current ticket alotment. On their next turn, they create a hypothesis as to the direction of the spy, and assume the spy has randomly walked to another node - each agent hypothesizes that the spy has moved to a node furthest away from this agent. They keep updating their guess at the path the spy is randomly walking, and keep using Yen's algorithm to approach their current hypothesis. When the spy is revealed in a later round, this guess is replaced with the reality, and the process continues.
 
 The spy moves accordingly: the spy measures all of the distances from all of the agents, but doesn't account for the ticket alotments - it simply calculates how "far" an agent is from the spy in terms of shortest path distance for the union of the taxi, bus, and metro network. It then measures the average shortest path length for every node it could move to, and then moves to a node that is maximally distant by this metric. It continuously moves using this rule until the spy is either caught or the adversaries have lost due to round exhaustion or their inability to move any more.
 
-##Upcoming
-Currently, the code records metadata for each game in terms of the transits agents take and the ultimate outcome of the match. This will then be used to analyze the general dynamics of the game for agents playing under the terms described above.
+##Results
+5,960 games were simulated using the above rules, resulting in 3,738 Spy wins and 2,222 Adversary wins. From the recordings of the matches, a few statistical descriptions were generated. First, a map of the game:
+
+![Map Of Scotland Yard](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/visualization.png)
+
+The nodes are colored by their community according to a modularity maximization algorithm, the edges are colored by the type of transit that connects the nodes.
+
+![Winning ratio per start position](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/spy_start_odds.png)
+
+Surprisingly, the starting position for the Spy appears to matter - starting on Node 34 greatly increases the odds of winning versus Node 26, where more often than not the Spy will lose. The first three transitions were sorted into a machine learning program, but the algorithm could only correctly predict 65% of the matches, so these results aren't reported here (though the code for that is in predictor.py). 
+
+Spy's that win tend towards the Metro stations and then towards the extremities, but don't linger in the outer core of the network (darker red indicates that the Spy transits through this area more - this is a composite graph of all transits for all Spy's that won their round):
+
+
+![Winning Spy Transitions](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/spy_win_transit_density.png)
+
+Adversaries stick more around central high transit nodes, as they are more likely to be hypothesized during turns of Spy ambiguity: 
+
+![Winning Adversaries Transitions](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/spy_win_transit_density.png)
+
+Transits per node are highly linear when plotted by total Adversary vs Spy transits:
+
+![Transit Covariance](https://raw.githubusercontent.com/DGaffney/scotland_yard/master/results/transit_relationship.png)
+
+There's likely more here to unpack, but for now, it's well past the weekend, and this is a weekend joke project... So feel free to modify and adapt the code to find any more interesting patterns (DISCLAIMER: This is weekend project code, there are probably bugs here and there). To get a copy of around 9,000 games, please download the pickle file on my site [here](http://devingaffney.com/files/9k_games.pkl).
--- strategy_game.py
+++ strategy_game.py
@@ -1,3 +1,5 @@
+import pickle
+import random
 import operator
 import igraph
 import csv
@@ -6,6 +8,7 @@ import json
 import copy
 import queue
 import yen_k_shortest
+from collections import Counter
 def read_csv(filename):
   return [[int(i) for i in str.split(el, ",")] for el in str.split(str(open(filename, 'r').read()), "\n")]
 
@@ -27,6 +30,7 @@ metro_edges = read_csv("data/metro_map.txt")
 ferry_edges = read_csv("data/ferry_map.txt")
 start_info = json.loads(open("data/start_data.json").read())
 graph_types = ["taxi", "bus", "metro", "ferry"]
+
 def moves_for_player(player, adversaries, graphs):
   adversary_positions = [a["position"] for a in adversaries]
   possible_methods = [g for g in graph_types if player['moves'][g] != 0]
@@ -67,8 +71,9 @@ def calculate_shortest_ticket_path_next_move(adversary, adversaries, spy, graphs
     adversary['spy_guess'] = spy['position']
   else:
     possible_moves = list(set(graphs[graph_types.index(spy["last_transit_type"])].neighbors(spy['position']))-set([adversary["position"]]))
-    np.random.shuffle(possible_moves)
-    adversary['spy_guess'] = possible_moves[0]
+    distances_from_me = [len(full_adversary_graph.get_shortest_paths(adversary["position"], possible_move)[0]) for possible_move in possible_moves]
+    max_distance_from_me = sorted(distances_from_me)[-1]
+    adversary['spy_guess'] = random.choice([pm for k,pm in enumerate(possible_moves) if distances_from_me[k] == max_distance_from_me])
   strategies = yen_k_shortest.yen_igraph(tmp_graph, adversary["position"], adversary['spy_guess'], 10, None)[0]
   gz = [strategies.append(strat) for strat in yen_k_shortest.yen_igraph(taxi_graph, adversary["position"], adversary['spy_guess'], 3, None)[0]]
   transit_methods = []
@@ -207,7 +212,27 @@ ferry_graph = fill_in_graph(ferry_edges, nodes)
 spy_wins = 0
 adversary_wins = 0
 all_game_data = []
-for i in range(1000):
+spy_win_record = []
+full_adversary_graph = igraph.Graph()
+full_adversary_graph.add_vertices(nodes)
+fa_edges = []
+for edge in taxi_edges:
+  if edge not in fa_edges:
+    fa_edges.append(edge)
+
+for edge in bus_edges:
+  if edge not in fa_edges:
+    fa_edges.append(edge)
+
+for edge in bus_edges:
+  if edge not in fa_edges:
+    fa_edges.append(edge)
+
+for edge in fa_edges:
+  indices = [nodes.index(n) for n in edge]
+  full_adversary_graph.add_edges([(indices[0], indices[1])])
+
+for i in range(10000):
   #IDEA - every adversary has an internal current guess for where spy is, and spy has a flag on the obj which shows previous transit type
   taxi_graph = fill_in_graph(taxi_edges, nodes)
   bus_graph = fill_in_graph(bus_edges, nodes)
@@ -218,7 +243,8 @@ for i in range(1000):
   np.random.shuffle(start_info["start_positions"])
   spy_start = start_info["start_positions"][0]-1
   adversary_starts = list(np.array(start_info["start_positions"][1:7])-1)
-  adversaries = [{"transits": [a_s], "transit_types": [], "spy_guess": None, "role": "adversary", "state": "alive", "position": a_s, "moves": copy.deepcopy(start_info["tickets"]["adversaries"])} for a_s in adversary_starts]
+  possible_starts = list(set(np.array(start_info["start_positions"])-1)-set(adversary_starts))
+  adversaries = [{"transits": [a_s], "transit_types": [], "spy_guess": random.choice(possible_starts)-1, "role": "adversary", "state": "alive", "position": a_s, "moves": copy.deepcopy(start_info["tickets"]["adversaries"])} for a_s in adversary_starts]
   spy = {"transits": [spy_start], "transit_types": [], "role": "spy", "state": "alive", "position": spy_start, "moves": copy.deepcopy(start_info["tickets"]["spy"])}
   moves = 0
   adversary_moves = []
@@ -233,8 +259,10 @@ for i in range(1000):
     moves += 1
   if spy["position"] not in [a["position"] for a in adversaries]:
     spy_wins += 1
+    spy_win_record.append(1)
   else:
     adversary_wins += 1
+    spy_win_record.append(0)
   if moves == 23 and spy["position"] not in [a["position"] for a in adversaries]:
     spy_win_hits += np.array([graph.vs["spy_transits"] for graph in graphs]).sum(0)
     adversary_lose_hits += np.array([graph.vs["adversary_transits"] for graph in graphs]).sum(0)
@@ -242,6 +270,117 @@ for i in range(1000):
     spy_lose_hits += np.array([graph.vs["spy_transits"] for graph in graphs]).sum(0)
     adversary_win_hits += np.array([graph.vs["adversary_transits"] for graph in graphs]).sum(0)
   all_game_data.append({'iteration': i, 'adversaries': adversaries, 'spy': spy, 'graphs': graphs})
+  print([spy_wins, adversary_wins])
 
-[spy_wins, adversary_wins]
 
+def analyze_game(all_game_data, spy_win_record):
+  station_data = [str.split(el, ",") for el in str.split(open("data/nodelist.csv").read(), "\r\n")]
+  station_dict = {}
+  for name,st_type in station_data:
+    station_dict[name] = st_type
+  win_heatmap = {}
+  loss_heatmap = {}
+  adversary_starting_spot_wins = {}
+  adversary_starting_spot_losses = {}
+  spy_starting_spot_wins = {}
+  spy_starting_spot_losses = {}
+  adversary_heatmap_wins = {}
+  adversary_heatmap_losses = {}
+  spy_heatmap_wins = {}
+  spy_heatmap_losses = {}
+  adversary_timeline_positions_wins = {}
+  adversary_timeline_positions_losses = {}
+  spy_timeline_positions_wins = {}
+  spy_timeline_positions_losses = {}
+  adversary_timeline_positions_wins_generalized = {}
+  adversary_timeline_positions_losses_generalized = {}
+  spy_timeline_positions_wins_generalized = {}
+  spy_timeline_positions_losses_generalized = {}
+  spy_total_transits_heatmap_win = {}
+  spy_total_transits_heatmap_loss = {}
+  adversary_total_transits_heatmap_win = {}
+  adversary_total_transits_heatmap_loss = {}
+  for heatmap in [adversary_total_transits_heatmap_loss, adversary_total_transits_heatmap_win, spy_total_transits_heatmap_win, spy_total_transits_heatmap_loss, adversary_heatmap_wins, adversary_heatmap_losses, spy_heatmap_wins, spy_heatmap_losses, adversary_starting_spot_wins, adversary_starting_spot_losses, spy_starting_spot_wins, spy_starting_spot_losses]:
+    for val in list(set(np.array(np.arange(200)+1).tolist())-set([108])):
+      heatmap[str(val)] = 0
+      win_heatmap[str(val)] = 0
+      loss_heatmap[str(val)] = 0
+  for heatmap in [adversary_timeline_positions_wins, adversary_timeline_positions_losses, spy_timeline_positions_wins, spy_timeline_positions_losses]:
+    for val in list(set(np.array(np.arange(200)+1).tolist())-set([108])):
+      heatmap[str(val)] = {}
+      for i in np.arange(24):
+        heatmap[str(val)][str(i)] = 0
+  for heatmap in [adversary_timeline_positions_wins_generalized, adversary_timeline_positions_losses_generalized, spy_timeline_positions_wins_generalized, spy_timeline_positions_losses_generalized]:
+    for i in np.arange(24):
+      heatmap[str(i)] = {"metro": 0, "bus": 0, "taxi": 0, "ferry": 0}
+  win_games = []
+  loss_games = []
+  for i,game in enumerate(all_game_data):
+    if spy_win_record[i] == 0:
+      loss_games.append(game)
+    else:
+      win_games.append(game)
+  for win_game in win_games:
+    spy_heatmap_wins[str(win_game["spy"]["position"]+1)] += 1
+    spy_starting_spot_wins[str(win_game["spy"]["transits"][0]+1)] += 1
+    for p in [a["position"] for a in win_game["adversaries"]]:
+      adversary_heatmap_wins[str(p+1)] += 1
+    for p in [a["transits"][0] for a in win_game["adversaries"]]:
+      adversary_starting_spot_wins[str(p+1)] += 1
+    for transit in [a["transits"] for a in win_game["adversaries"]]:
+      for s,step in enumerate(transit):
+        adversary_timeline_positions_wins[str(step+1)][str(s)] += 1
+        adversary_total_transits_heatmap_win[str(step+1)] += 1
+        adversary_timeline_positions_wins_generalized[str(s)][station_dict[str(step+1)]] += 1
+    for s,step in enumerate(win_game["spy"]["transits"]):
+      spy_timeline_positions_wins[str(step+1)][str(s)] += 1
+      spy_total_transits_heatmap_win[str(step+1)] += 1
+      spy_timeline_positions_wins_generalized[str(s)][station_dict[str(step+1)]] += 1
+  for loss_game in loss_games:
+    spy_heatmap_losses[str(loss_game["spy"]["position"]+1)] += 1
+    spy_starting_spot_losses[str(loss_game["spy"]["transits"][0]+1)] += 1
+    for p in [a["position"] for a in loss_game["adversaries"]]:
+      adversary_heatmap_losses[str(p+1)] += 1
+    for p in [a["transits"][0] for a in loss_game["adversaries"]]:
+      adversary_starting_spot_losses[str(p+1)] += 1
+    for transit in [a["transits"] for a in loss_game["adversaries"]]:
+      for s,step in enumerate(transit):
+        adversary_timeline_positions_losses[str(step+1)][str(s)] += 1
+        adversary_total_transits_heatmap_loss[str(step+1)] += 1
+        adversary_timeline_positions_losses_generalized[str(s)][station_dict[str(step+1)]] += 1
+    for s,step in enumerate(loss_game["spy"]["transits"]):
+      spy_timeline_positions_losses[str(step+1)][str(s)] += 1
+      spy_total_transits_heatmap_loss[str(step+1)] += 1
+      spy_timeline_positions_losses_generalized[str(s)][station_dict[str(step+1)]] += 1
+  visualization_dataset = [["node_id", "spy_win_density", "spy_loss_density", "adversary_win_density", "adversary_loss_density", "spy_win_adversary_start_density", "spy_lose_adversary_start_density", "spy_win_spy_start_density", "spy_lose_spy_start_density","spy_total_transits_heatmap_win","spy_total_transits_heatmap_lose","adversary_total_transits_heatmap_win","adversary_total_transits_heatmap_lose"]]
+  ml_dataset = [["spy_wins", "spy_start", "adversary_start_1", "adversary_start_2", "adversary_start_3", "adversary_start_4", "adversary_start_5", "adversary_start_6"]]
+  generalized_transit_type_dataset = [["turn", "spy_win_avg_metro_count_spy", "spy_win_avg_bus_count_spy", "spy_win_avg_taxi_count_spy", "adversary_win_avg_metro_count_spy", "adversary_win_avg_bus_count_spy", "adversary_win_avg_taxi_count_spy", "spy_loss_avg_metro_count_spy", "spy_loss_avg_bus_count_spy", "spy_loss_avg_taxi_count_spy", "adversary_loss_avg_metro_count_spy", "adversary_loss_avg_bus_count_spy", "adversary_loss_avg_taxi_count_spy"]]
+  for turn in np.arange(24):
+    stw = [spy_timeline_positions_wins_generalized[str(turn)]["metro"], spy_timeline_positions_wins_generalized[str(turn)]["bus"], spy_timeline_positions_wins_generalized[str(turn)]["taxi"]]
+    atw = [adversary_timeline_positions_wins_generalized[str(turn)]["metro"], adversary_timeline_positions_wins_generalized[str(turn)]["bus"], adversary_timeline_positions_wins_generalized[str(turn)]["taxi"]]
+    stl = [spy_timeline_positions_losses_generalized[str(turn)]["metro"], spy_timeline_positions_losses_generalized[str(turn)]["bus"], spy_timeline_positions_losses_generalized[str(turn)]["taxi"]]
+    atl = [adversary_timeline_positions_losses_generalized[str(turn)]["metro"], adversary_timeline_positions_losses_generalized[str(turn)]["bus"], adversary_timeline_positions_losses_generalized[str(turn)]["taxi"]]
+    if sum(stw) != 0:
+      stw = [s/float(sum(stw)) for s in stw]
+    if sum(atw) != 0:
+      atw = [s/float(sum(atw)) for s in atw]
+    if sum(stl) != 0:
+      stl = [s/float(sum(stl)) for s in stl]
+    if sum(atl) != 0:
+      atl = [s/float(sum(atl)) for s in atl]
+    generalized_transit_type_dataset.append([turn,stw[0],stw[1],stw[2],atw[0],atw[1],atw[2],stl[0],stl[1],stl[2],atl[0],atl[1],atl[2]])
+  for node_id in list(set(np.array(np.arange(200)+1).tolist())-set([108])):
+    visualization_dataset.append([node_id, spy_heatmap_wins[str(node_id)], spy_heatmap_losses[str(node_id)], adversary_heatmap_wins[str(node_id)], adversary_heatmap_losses[str(node_id)],spy_starting_spot_wins[str(node_id)],spy_starting_spot_losses[str(node_id)],adversary_starting_spot_wins[str(node_id)],adversary_starting_spot_losses[str(node_id)],spy_total_transits_heatmap_win[str(node_id)],spy_total_transits_heatmap_loss[str(node_id)],adversary_total_transits_heatmap_win[str(node_id)],adversary_total_transits_heatmap_loss[str(node_id)]])
+  for game in win_games:
+    a_s = [a["transits"][0] for a in game["adversaries"]]
+    a_s1 = [a["transits"][1] for a in game["adversaries"]]
+    a_s2 = [a["transits"][2] for a in game["adversaries"]]
+    ml_dataset.append([1, game["spy"]["transits"][0], game["spy"]["transits"][1], game["spy"]["transits"][2], a_s[0], a_s[1], a_s[2], a_s[3], a_s[4], a_s[5], a_s1[0], a_s1[1], a_s1[2], a_s1[3], a_s1[4], a_s1[5], a_s2[0], a_s2[1], a_s2[2], a_s2[3], a_s2[4], a_s2[5]])
+  for game in loss_games:
+    a_s = [a["transits"][0] for a in game["adversaries"]]
+    a_s1 = [a["transits"][1] for a in game["adversaries"]]
+    a_s2 = [a["transits"][2] for a in game["adversaries"]]
+    ml_dataset.append([0, game["spy"]["transits"][0], game["spy"]["transits"][1], game["spy"]["transits"][2], a_s[0], a_s[1], a_s[2], a_s[3], a_s[4], a_s[5], a_s1[0], a_s1[1], a_s1[2], a_s1[3], a_s1[4], a_s1[5], a_s2[0], a_s2[1], a_s2[2], a_s2[3], a_s2[4], a_s2[5]])
+  with open('games.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerows(ml_dataset)
